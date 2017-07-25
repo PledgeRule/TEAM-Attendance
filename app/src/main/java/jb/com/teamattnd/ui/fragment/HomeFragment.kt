00ddfac3529072ef13074ed59d90aa7837fa374b
sandeep-singh-incedo <sandeep.singh@incedoinc.com>
2017-07-25 12:41:53 +0530

    Added Kotlin Support

---
+++ app/src/main/java/jb/com/teamattnd/ui/fragment/HomeFragment.kt
@@ -0,0 +1,49 @@
+package jb.com.teamattnd.ui.fragment
+
+import android.icu.lang.UCharacter
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.ListFragment
+import android.view.*
+import jb.com.teamattnd.R
+import kotlinx.android.synthetic.main.activity_home.*
+
+/**
+ * Created by sandeep.singh on 7/25/2017.
+ */
+class HomeFragment :ListFragment(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        setHasOptionsMenu(true)
+        return super.onCreateView(inflater, container, savedInstanceState)
+
+
+    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater!!.inflate(R.menu.bluetooth_menu,menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+         super.onOptionsItemSelected(item)
+
+        when(item!!.itemId){
+            R.id.secure_connect_scan-> println("Secure connection")
+            R.id.insecure_connect_scan-> println(" In Secure connection")
+            R.id.discoverable-> println("make discoverable")
+        }
+
+        return true
+    }
+
+}
